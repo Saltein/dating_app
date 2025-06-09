5a62e4bca3919fc78da13c8bc5f15994a68fee11
Никита <nikitval@list.ru>
2025-06-10 00:16:17 +0300

    анимации лайков

--- src/shared/ui/Buttons/LikeButton/LikeButton.jsx
+++ src/shared/ui/Buttons/LikeButton/LikeButton.jsx
@@ -2,9 +2,25 @@ import s from './LikeButton.module.scss'
 import { ReactComponent as LikeIcon } from '../../../assets/icons/heart.svg'
 import { ReactComponent as SuperLikeIcon } from '../../../assets/icons/fire.svg'
 import { ReactComponent as DislikeIcon } from '../../../assets/icons/cancel.svg'
+import { useEffect, useState } from 'react'
 
 // type = 'like' | 'dislike' | 'super'
 export const LikeButton = ({ type = 'like', isBig = true, onClick }) => {
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    const handleClick = () => {
+        onClick()
+        setIsClicked(true)
+        setTimeout(() => {
+            setIsClicked(false)
+        }, 600)
+    }
+
+    useEffect(()=>{
+        console.log('isClicked', isClicked)
+    }, [isClicked])
+
     let Icon
 
     if (type === 'like') {
@@ -18,7 +34,7 @@ export const LikeButton = ({ type = 'like', isBig = true, onClick }) => {
     }
 
     return (
-        <button className={`${s.wrapper} ${type === 'like' ? s.like : ''} ${type === 'dislike' ? s.dislike : ''}`} onClick={onClick}>
+        <button className={`${s.wrapper} ${type === 'like' ? s.like : ''} ${type === 'dislike' ? s.dislike : ''} ${isClicked ? s.clicked : ''}`} onClick={handleClick}>
             {
                 <Icon className={`${s.icon} ${isBig ? s.big : s.small}`} />
             }
